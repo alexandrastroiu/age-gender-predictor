daf83cd252d4768d134944b468210fc52a0a5e59
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-03-23 19:12:53 +0200

    Add script for handling webcam
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ app/camera.py
@@ -0,0 +1,26 @@
+import cv2
+
+class Camera:
+    def __init__(self, webcam_index = 0):
+        self.index = webcam_index
+        self.capture = None
+    
+    def start_camera(self):
+        self.capture = cv2.VideoCapture(self.index)
+
+        if not self.capture.isOpened():
+            raise RuntimeError("Error: Webcam cannot be opened")
+    
+    def read_frame(self):
+        if self.capture is None:
+            raise RuntimeError("Webcam not started")
+
+        ret, frame = self.capture.read()
+
+        if not ret:
+            raise RuntimeError("Error: Could not read frame from webcam")
+
+    def close_camera(self):
+        if self.capture is not None:
+            self.capture.release()
+            self.capture = None
