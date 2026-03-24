0af49336836ce5730d341cd229887eb0f183cd84
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-03-24 12:01:32 +0200

    Add function for extracting largest face in webcam stream
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- app/face_detector.py
+++ app/face_detector.py
@@ -8,3 +8,12 @@ class FaceDetector:
          gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
          faces = self.detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(60, 60)) 
          return faces
+    
+    def select_largest_face(self, faces, frame):
+        if len(faces) == 0:
+            return None
+        
+        col, row, width, height = max(faces, key=lambda face: face[2] * faces[3])
+        largest_face = frame[row:row + height, col: col + width]
+        return largest_face
+        
