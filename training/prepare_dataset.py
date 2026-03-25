1f5ec4cdc6bd9bde1cf4f57e9537b0f200d81755
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-03-25 16:48:43 +0200

    Modify dataset loading script
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ training/prepare_dataset.py
@@ -0,0 +1,16 @@
+import cv2
+import os
+import numpy as np
+
+DATASET_PATH = "./../data/UTKFace"
+SIZE = (224, 224)
+AGE_CATEGORY = ""
+
+def load_dataset():
+    pass
+
+def preprocess_image():
+    pass
+
+def get_labels():
+    pass
