3b34086eff0fbefe9aea1a3bd739b2a3eba6c65d
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-04-05 12:46:52 +0300

    Add preprocessing for image
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- prediction.py
+++ prediction.py
@@ -1,14 +1,22 @@
 import numpy as np
 import cv2
 import tensorflow as tf
-from keras.models import load_models
+from keras.models import load_model
+
+MODEL_PATH = "./../models/age_gender_model.keras"
+SIZE = (224, 224)
 
 class Prediction:
-    def __init__():
-        pass
+    def __init__(self):
+        self.model = load_model(MODEL_PATH)
 
-    def preprocess():
-        pass
+    def preprocess(self, image):
+        image = cv2.resize(image, SIZE)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image.astype("float32") / 255.0
+        image = np.expand_dims(image, axis=0)
 
-    def predict():
+        return image
+
+    def predict(self, image):
         pass
