3ce560d67ed77d999f0cc9f06b587087846dfb52
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-04-05 12:40:25 +0300

    Add script for prediction
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ prediction.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+import tensorflow as tf
+from keras.models import load_models
+
+class Prediction:
+    def __init__():
+        pass
+
+    def preprocess():
+        pass
+
+    def predict():
+        pass
