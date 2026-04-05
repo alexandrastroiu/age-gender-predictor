93fae46d6510fcbdccdda6ee29822ae32a72ec69
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-04-05 13:06:50 +0300

    Add prediction test in main
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- prediction.py
+++ prediction.py
@@ -4,7 +4,17 @@ import tensorflow as tf
 from keras.models import load_model
 
 MODEL_PATH = "./../models/age_gender_model.keras"
+TEST_IMAGE_PATH = "./../data/faces/"
 SIZE = (224, 224)
+AGE_CATEGORY = [
+    "0 - 12",
+    "13 - 19",
+    "0 - 29",
+    "30 - 45",
+    "46 - 60",
+    "60 - 75",
+    "75 +"
+]
 
 class Prediction:
     def __init__(self):
@@ -19,4 +29,21 @@ class Prediction:
         return image
 
     def predict(self, image):
-        pass
+        image = self.preprocess(image)
+        gender_pred, age_pred = self.model.predict(image)
+        gender = "Female" if gender_pred[0][0] else "Male"
+        age_category = AGE_CATEGORY[np.argmax(age_pred[0])]
+        return {
+            "gender": gender,
+            "gender confidence": gender_pred[0][0],
+            "age category": age_category,
+            "age confidence": float(np.max(age_pred[0]))
+        }
+    
+# Test prediction on one image
+def main():
+    prediction = Prediction()
+    print(prediction.predict(TEST_IMAGE_PATH))
+
+if __name__=="__main__":
+    main()
