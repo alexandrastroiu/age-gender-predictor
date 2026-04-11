1c32c093abfd3082d0a48c9cd97a6ac3c13c000d
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-04-11 13:52:11 +0300

    Add preediction functionality in gui
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- tkinter_ui.py
+++ tkinter_ui.py
@@ -11,9 +11,11 @@ root = tk.Tk()
 camera = Camera()
 face_detector = FaceDetector()
 prediction = Prediction()
+current_frame = None
+current_face = None
 
 root.title("Age & Gender Prediction App")
-root.configure(bg='#E5E5FF')
+root.configure(bg='#D8D6FF')
 
 menu = tk.Menu(root)
 root.config(menu=menu)
@@ -33,20 +35,26 @@ label1 = tk.Label(root, text="Welcome!", bg="lightgray", fg="black")
 label1.pack()
 
 label_img = tk.Label(master=root)
-label_img.configure(bg='#E5E5FF')
+label_img.configure(bg='#D8D6FF')
 label_img.pack(pady=40)
 label_img.place(relx=0.5, rely=0.5, relwidth=0.6, relheight=0.6, anchor = 'e')
+
+label_result = tk.Label(master=root)
+label_result.configure(bg='white')
+label_result.pack(pady=40)
+label_result.place(relx=0.5, rely=0.5, relwidth=0.6, relheight=0.6, anchor = 'w')
 
 camera.start_camera()
 
 def update_camera():
+    global current_frame, current_face
     try:
         frame = camera.read_frame()
+        current_frame = frame.copy()
         faces = face_detector.detect_face(frame)
+        current_face = faces
         for (x, y, w, h) in faces:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        largest_face = face_detector.select_largest_face(faces, frame)
         
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         resized_img = cv2.resize(img, (820, 500))
@@ -59,11 +67,28 @@ def update_camera():
     root.after(250, update_camera) #TODO change update time (250 ms)
 
 
+def get_prediction():
+    largest_face = face_detector.select_largest_face(current_face, current_frame)
+ 
+    if largest_face is None:
+        label_result.config(text="No face detected")
+        return
+    
+    result = prediction.predict(largest_face)
+    label_result.config(text=result)
+
+button = tk.Button(root, text="Get Prediction", command=get_prediction, activebackground="darkviolet")
+button.pack(pady=20)
+
+
+
+
 def close():
     camera.close_camera()
     root.destroy()
 
 update_camera()
+
 
 
 
