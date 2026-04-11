5de1c62aca1ad04346fe90d17baa563a903e063e
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-04-11 17:41:38 +0300

    Change results label
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- tkinter_ui.py
+++ tkinter_ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font
 from PIL import Image, ImageTk
 from app.camera import Camera
 from app.face_detector import FaceDetector
@@ -15,34 +16,55 @@ current_frame = None
 current_face = None
 
 root.title("Age & Gender Prediction App")
-root.configure(bg='#D8D6FF')
+root.configure(bg="#EAE8F8")
+
+font = tkinter.font.Font(family="Helvetica", size=14,)
+font_header = tkinter.font.Font(family="Helvetica", size=16, weight="bold")
+font_title = tkinter.font.Font(family="Helvetica", size=32, weight="bold")
 
 menu = tk.Menu(root)
 root.config(menu=menu)
 
 filemenu = tk.Menu(menu)
-menu.add_cascade(label="Instructions", menu=filemenu)
+menu.add_cascade(label="Instructions", menu=filemenu, font=font)
 filemenu.add_command(label="""
             * Upload a picture from your computer or capture a snapshot from your camera
             * Click the 'Run prediction' button to see your predicted age and gender
-            * Click the 'Save snapshot' button to save pictures to your own device
-             """)
+            * Click the 'Share results' button to hare results online
+             """, font=font)
 
-label = tk.Label(root, text="Age & Gender Prediction App", bg="lightgray", fg="black")
+label = tk.Label(root, text="Age & Gender Prediction App", bg="#EAE8F8",fg="#2C2C2C", font=font_title)
 label.pack(pady=40)
 
-label1 = tk.Label(root, text="Welcome!", bg="lightgray", fg="black")
+
+label1 = tk.Label(root, text="Welcome!", bg="#EAE8F8",fg="#2C2C2C", font=font_header)
 label1.pack()
 
+separator = tk.Frame(root, height=2, bg="#2C2C2C")
+separator.pack(fill="x", pady=10)
+
 label_img = tk.Label(master=root)
-label_img.configure(bg='#D8D6FF')
+label_img.configure(bg='#EAE8F8')
 label_img.pack(pady=40)
 label_img.place(relx=0.5, rely=0.5, relwidth=0.6, relheight=0.6, anchor = 'e')
 
-label_result = tk.Label(master=root)
+label_result = tk.Label(master=root, text="Prediction Results", font=font_header, anchor="n")
 label_result.configure(bg='white')
 label_result.pack(pady=40)
-label_result.place(relx=0.5, rely=0.5, relwidth=0.6, relheight=0.6, anchor = 'w')
+label_result.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.53, anchor = 'w')
+
+box_gender = tk.Label(label_result, text="Gender: ", width=50, height=2, relief="solid", bd=1, anchor="center", font=font, bg="white")
+box_gender.grid(row=0, column=0, padx=50, pady=50)
+
+box_gender_conf = tk.Label(label_result, text="Gender confidence: ", width=50, height=2, relief="solid", bd=1, anchor="center",font=font, bg="white")
+box_gender_conf.grid(row=1, column=0, padx=50, pady=25)
+
+box_age = tk.Label(label_result, text="Age Range: ", width=50, height=2, relief="solid", bd=1, anchor="center", font=font, bg="white")
+box_age.grid(row=2, column=0, padx=50, pady=25)
+
+box_age_conf = tk.Label(label_result, text="Age confidence: ", width=50, height=2, relief="solid", bd=1, anchor="center", font=font, bg="white")
+box_age_conf.grid(row=3, column=0, padx=50, pady=25)
+
 
 camera.start_camera()
 
@@ -57,14 +79,14 @@ def update_camera():
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        resized_img = cv2.resize(img, (820, 500))
+        resized_img = cv2.resize(img, (850, 500))
         img = Image.fromarray(resized_img)
         imgtk = ImageTk.PhotoImage(image=img)
         label_img.imgtk = imgtk
         label_img.configure(image=imgtk)
     except Exception as e:
         print("Camera error:", e)
-    root.after(250, update_camera) #TODO change update time (250 ms)
+    root.after(250, update_camera)
 
 
 def get_prediction():
@@ -75,12 +97,10 @@ def get_prediction():
         return
     
     result = prediction.predict(largest_face)
-    label_result.config(text=result)
-
-button = tk.Button(root, text="Get Prediction", command=get_prediction, activebackground="darkviolet")
-button.pack(pady=20)
-
-
+    box_gender.config(text=f"Gender: {result["Gender"]}")
+    box_gender_conf.config(text=f"Gender Confidence: {(result["Gender Confidence"] * 100):.2f} %")
+    box_age.config(text=f"Age Range: {result["Age Category"]}")
+    box_age_conf.config(text=f"Age Confidence: {result["Age Confidence"] * 100 :.2f} %")
 
 
 def close():
@@ -89,8 +109,15 @@ def close():
 
 update_camera()
 
-quit_button = tk.Button(root, text="Quit Age & Gender Prediction App", command=close)
-quit_button.pack(pady=20)
+
+quit_button = tk.Button(root, text="Quit Age & Gender Prediction App", command=close, font=font_header, width=30, height=1)
+quit_button.pack(side="bottom", pady=10)
+
+share_button = tk.Button(root, text="Share Results", font=font_header, width=30, height=1)
+share_button.pack(side="bottom", pady=10)
+
+button = tk.Button(root, text="Get Prediction", command=get_prediction, font=font_header, width=30, height=1)
+button.pack(side="bottom", pady=10)
 
 
 root.protocol("WM_DELETE_WINDOW", close)
