d7f2fb3b8ae611bf9ebe7833316459996659fbd1
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-04-11 17:41:21 +0300

    Fic age confidence bug
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- app/prediction.py
+++ app/prediction.py
@@ -7,11 +7,11 @@ from app.preprocessing import preprocess_face
 MODEL_PATH = "./models/age_gender_model.keras"
 SIZE = (224, 224)
 AGE_CATEGORY = [
-    (0,12),
-    (13, 24),
-    (25, 39),
-    (40, 59),
-    (60, 120),
+    "0 - 12",
+    "13 - 24",
+    "25 - 39",
+    "40 - 59",
+    "60 - 120",
 ]
 
 class Prediction:
@@ -22,10 +22,11 @@ class Prediction:
         image = preprocess_face(image)
         gender_pred, age_pred = self.model.predict(image)
         gender = "Female" if gender_pred[0][0] > 0.5 else "Male"
+        gender_confidence = gender_pred[0][0] if gender == "Female" else 1 - gender_pred[0][0]
         age_category = AGE_CATEGORY[np.argmax(age_pred[0])]
         return {
             "Gender": gender,
-            "Gender Confidence": gender_pred[0][0],
+            "Gender Confidence": gender_confidence,
             "Age Category": age_category,
             "Age Confidence": float(np.max(age_pred[0]))
         }
